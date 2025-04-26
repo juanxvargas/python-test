478aae170ac64e9e06a74517447b9181b2d1b668
juanxvargas <134565211+juanxvargas@users.noreply.github.com>
2025-04-26 11:47:51 -0400

    Update server.py

--- server.py
+++ server.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
 from PIL import Image
 import io
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las fuentes (puedes poner un dominio específico si quieres más seguridad)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
